c02f9ab4386fcd9f034976453005853c5e483db4
NessySweet <va.ogbuehi@gmail.com>
2017-04-03 17:14:19 -0400

    Edit menus

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php include_once('templates/head.php');?>
 <?php include_once('templates/header.php');?>
 <?php include_once('templates/sidebar.php');?>
+
+<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="dist/js/app.min.js"></script>
