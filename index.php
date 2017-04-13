9811b53d63239310e0145ec487d3638335c01d5f
NessySweet <va.ogbuehi@gmail.com>
2017-04-13 06:56:59 -0400

    Add composer.json and Procfile

--- index.php
+++ index.php
@@ -11,5 +11,5 @@ $city = $_POST["city"];
 $admin_sql = "INSERT INTO admin (Username, First_Name, Last_Name, Email, Country, City) VALUES ( '".$username."', '".$first_name."', '".$last_name."', '".$email."', '".$country."', '".$city."')";
 
 mysqli_query($con,$admin_sql);
-header("Location: admin.php?")
+header("Location: /admin.php?")
 ?>
